8a2872f0e187de469626b7202bcc32c3d0455e9b
emarinfer <enriquemarin_sierra@hotmail.com>
2021-06-04 09:53:45 +0200

    Added documentation, device screenshot and README.md file

---
+++ README.md
@@ -0,0 +1,8 @@
+# My Landing Page
+------------
+
+***Device screenshot:***
+
+<img src="./img/pixel-2-xl-front.png" width="700"/>
+
+### Mi Sitio Web. Incluye mi presentación y mi curriculum.
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -57,3 +57,31 @@ module.exports = {
         }
       }
   }
+
+
+  /**
+   * DOCUMENTACION:
+   * 
+   * SOBRE @MR-HOPE/VUEPRESS-PLUGIN-PWA (ES EL PLUGIN QUE FUNCIONA CON VUEPRESS):
+   * https://vuepress-theme-hope.github.io/pwa/
+   * 
+   * SOBRE EL PLUGIN PWA DE VUEPRESS:
+   * https://vuepress2.netlify.app/reference/plugin/pwa.html#web-app-manifests
+   * 
+   * DOCUMENTACION OFICIAL DE VUEPRESS SOBRE EL PLUGIN PWA:
+   * https://vuepress.vuejs.org/plugin/official/plugin-pwa.html
+   * 
+   * DESPLIEGUE DE UNA SPA REALIZADA CON VUEPRESS EN NETLIFY:
+   * https://blog.michaelbrooks.dev/deploying-your-vuepress-blog-to-netlify/
+   * 
+   * PROBLEMAS EN EL DESPLIEGUE EN NETLIFY:
+   * https://github.com/nuxt/nuxt.js/issues/6823#issuecomment-568435309
+   * 
+   * SOBRE EL PACKAGE.LOCK.JSON:
+   * https://rubensa.wordpress.com/2019/11/27/pero-que-demonios-es-package-lock-json/
+   * https://elabismodenull.wordpress.com/2017/07/07/el-fichero-package-lock-json-tengo-que-versionarlo/
+   * 
+   * DESPLIEGUE EN GITHUB PAGES:
+   * https://learnvue.co/2020/09/how-to-deploy-your-vue-app-to-github-pages/
+   * https://www.smashingmagazine.com/2016/08/sghpa-single-page-app-hack-github-pages/
+   */
